612626c8a1d617f1b7c2b948cb4f0280ea66c567
Boris Filipovic <boris.filipovic1@gmail.com>
2020-04-19 22:35:37 +0200

    Added rocket script.

---
+++ Project Booster/Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    // Private.
+    Rigidbody rigidBody;
+
+    // Public.
+
+	// Use this for initialization
+	void Start () {
+        // Find ships rigidbody reference.
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    // Handle user imputs.
+    private void ProcessInput()
+    {
+        // Check if boost button was pressed.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // Check if left or right button was pressed.
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if(Input.GetKey(KeyCode.D)) 
+        {
+            print("Rotating right");
+        }
+    }
+}
